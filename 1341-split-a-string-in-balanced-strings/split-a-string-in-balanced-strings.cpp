86878bbeb301cecc274d81b5a379a1464ba6e575
Amulya Ajay <amulyaajay251@gmail.com>
2026-09-02 14:28:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (57.55%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int balance = 0;
+        int count = 0;
+        for(char c : s){
+            if(c == 'L')
+                balance++;
+            if(c == 'R')
+                balance--;
+            if(balance == 0)
+                count++;
+        }
+        return count;
+    }
+};
